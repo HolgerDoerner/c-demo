67a2ccad6d40c96a5cc85442b67d992e836c12f4
Holger Dörner <hdoerner@fedora-workstation.fritz.box>
2019-08-21 23:20:22 +0200

    just a test

--- src/main.c
+++ src/main.c
@@ -1,21 +1,25 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
+#include <stdio.h> /* printf, sprintf */
+#include <stdlib.h> /* exit, atoi, malloc, free */
+#include <unistd.h> /* read, write, close */
+#include <string.h> /* memcpy, memset */
+#include <sys/socket.h> /* socket, connect */
+#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
+#include <netdb.h> /* struct hostent, gethostbyname */
 
 int main(int argc, char** argv) {
-	if (argc > 1) {
-		for (int i = 1; i < argc; i++) {
-			printf("%s\r\n", argv[i]);
-		}
-	}
+   if (argc == 1) {
+       printf("%s <city>\r\n", argv[0]);
+       return 0;
+    }
 
-	char* msg;
-	msg = "test";
+    int port = 80;
+    char *host = "https://goweather.herokuapp.com";
+    size_t strsize = strlen(host) + strlen("/weather/") + strlen(argv[1]);
+    char *url = (char *)malloc(sizeof(char) * strsize+1);
+    snprintf(url, strsize+1, "%s/weather/%s", host, argv[1]);
+    puts("...");
 
-	puts(msg);
-
-	pid_t pid = fork();
-	if (pid != 0) printf("%d\r\n", getpid());
-
-	return 0;
+    puts(url);
+    
+    return 0;
 }
